59681fc2190613a5415de5f813df8a86416c444e
HeJunchao <571258767@qq.com>
2022-09-05 15:30:00 +0800

    Modify comment

--- test/MoblieNetV1.java
+++ test/MoblieNetV1.java
@@ -1,6 +1,8 @@
 package test;
 
+import Model.K210AddPadding;
 import Model.K210Conv2d;
+import Model.K210RemovePadding;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -26,10 +28,26 @@ public class MoblieNetV1 {
 
     @Test
     public void Simulator() throws Exception {
+
+//        填充
+//        ifmap = K210AddPadding.simulatorAddPadding(ifmap,1);
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(Arrays.toString(result[i]));
+//        }
+
+//        移除填充
+//        ifmap = K210RemovePadding.simulatorRemovePadding(ifmap,1);
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(Arrays.toString(result[i]));
+//        }
+
 //        DW卷积
         psum = K210Conv2d.SimulatorConv2d(ifmap,Weight);
+
 //        PW卷积
         result = K210Conv2d.SimulatorPWConv2d(psum,PWweight);
+
+//        输出结果
         for (int i = 0; i < result.length; i++) {
             System.out.println(Arrays.toString(result[i]));
         }
